236bcdec0dc3f20874452573d5788433bf641772
batuhan4644 <batuhan4644@gmail.com>
2020-11-10 10:08:46 +0300

    github proje

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    
+    <Navbar />
+    <hr />
+    <BodyMenu />
+    <hr />
+     <BilgiMenu />
+    <hr />
+    <Slide />
+    <hr />
+    <UyeKayit />
+    <hr />
+    <IslemBasarili />
+    <hr />
+    
+   <Filtre />
+    <hr />
+    <BenzerUrunler />
+    <hr />
+    <div class="container">
+      <Liste :elemanlar="kategoriler" />
+      <Liste :coklu="true" :elemanlar="markalar" />
+      <Liste :coklu="true" :elemanlar="fiyatAraliklari" />
+    </div>
+    
+    
+  </div>
+</template>
+<script>
+import Filtre from "./components/Filtre";
+import UyeKayit from "./components/UyeKayit";
+import IslemBasarili from "./components/IslemBasarili";
+import Navbar from "./components/Navbar.vue";
+import BenzerUrunler from "./components/BenzerUrunler.vue";
+import Liste from "./components/Liste.vue";
+import Slide from "./components/Slide.vue";
+import BilgiMenu from "./components/BilgiMenu.vue";
+import BodyMenu from "./components/BodyMenu.vue";
+
+
+
+export default {
+  data() {
+    return {
+      kategoriler: [
+        "Mobil & Aksesuar",
+        "Akıllı Saat & Bileklikler",
+        "Aydınlatma Sistemleri",
+        "Bilgisayar & Tablet",
+        "Küçük Ev Aletleri",
+        "Tc,Görüntü & Ses Sistemleri",
+      ],
+      markalar: [
+        "Acura",
+        "Ban-co",
+        "Dat",
+        "Dextel",
+        "Diamond",
+        "Emrtech",
+        "Everton",
+        "Fepe",
+        "Arçelik",
+        "Beko",
+        "Casper",
+        "Nvidia",
+      ],
+      fiyatAraliklari: [
+        "70₺ ve altı",
+        "70₺ - 200₺",
+        "200₺ - 250₺",
+        "250₺ - 350₺",
+        "350₺ - 500₺",
+        "500₺ - 1000₺",
+        "1000₺ - 10000₺",
+      ],
+    };
+  },
+  components: {
+    Filtre,
+    IslemBasarili,
+    UyeKayit,
+    Navbar,
+    BenzerUrunler,
+    Liste,
+    Slide,
+    BilgiMenu,
+    BodyMenu,
+  
+  },
+};
+</script>
+<style>
+</style>
---
+++ src/components/BodyMenu.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+      
+       <p>
+      <td><img class="btn" :src="Body1" >
+       </td> 
+     
+     <td> <img class="btn" :src="Body2" >
+      </td> 
+ 
+     <td> <img class="btn" :src="Body3" >  
+      </td> 
+
+   
+
+      <p>
+      <td><img class="btn" :src="Body4" >
+    </td> 
+     
+     <td> <img class="btn" :src="Body5" >
+    </td> 
+ 
+     <td> <img class="btn" :src="Body6"    >
+     </td> 
+    
+<p>
+
+       <td><img class="btn" :src="Body7" >
+       </td> 
+     
+     <td> <img class="btn" :src="Body8" >
+     </td> 
+ 
+     <td> <img class="btn" :src="Body9"    >
+      </td> 
+        </div>       
+</template>
+    
+<script>
+import Body1 from "../assets/Body/Body1.png";
+import Body2 from "../assets/Body/Body2.png";
+import Body3 from "../assets/Body/Body3.png";
+import Body4 from "../assets/Body/Body4.png";
+import Body5 from "../assets/Body/Body5.png";
+import Body6 from "../assets/Body/Body6.png";
+import Body7 from "../assets/Body/Body7.png";
+import Body8 from "../assets/Body/Body8.png";
+import Body9 from "../assets/Body/Body9.png";
+export default {
+  data() {
+    return {
+      Body1,Body2, Body3, Body4, Body5,Body6, Body7, Body8,Body9
+    };
+  },
+};
+
+</script>
+
+<style scoped>
+.btn {
+  width: 103%
+}
+
+</style>
